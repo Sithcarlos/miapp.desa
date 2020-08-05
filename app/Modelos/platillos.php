65b36cb5c3bf5b3834110719b4fae5bb901e7540
Sithcarlos <doncarlos@outlook.com>
2020-08-05 16:08:50 -0500

    Se agregan nuevos modelos: paquetes, restaurante, categorias, platillos

---
+++ app/Modelos/platillos.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Modelos;
+
+use Illuminate\Database\Eloquent\Model;
+
+class platillos extends Model {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'alergenos', 'categoria_id', 'descripcion', 'nombre', 'precio', 'restaurante_id', 'updated_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+    ];
+
+}
---
+++ app/Modelos/restaurantes.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Modelos;
+
+use Illuminate\Database\Eloquent\Model;
+
+class restaurantes extends Model {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'activo', 'descripcion', 'imagen_ruta', 'nombre', 'paquete_id', 'ruta', 'usuario_id', 'updated_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'activo' => 'boolean',
+    ];
+
+}
