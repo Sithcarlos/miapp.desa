b3371d6f08cbababd369f2314cd56dd8ce6ee39a
Sithcarlos <doncarlos@outlook.com>
2020-08-05 16:12:33 -0500

    N: controladores para : paquetes, restaurante, categorias, platillos

---
+++ app/Http/Controllers/PlatillosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\platillos;
+use Illuminate\Http\Request;
+
+class PlatillosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\platillos  $platillos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(platillos $platillos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\platillos  $platillos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(platillos $platillos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\platillos  $platillos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, platillos $platillos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\platillos  $platillos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(platillos $platillos)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RestaurantesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\restaurantes;
+use Illuminate\Http\Request;
+
+class RestaurantesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\restaurantes  $restaurantes
+     * @return \Illuminate\Http\Response
+     */
+    public function show(restaurantes $restaurantes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\restaurantes  $restaurantes
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(restaurantes $restaurantes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\restaurantes  $restaurantes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, restaurantes $restaurantes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\restaurantes  $restaurantes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(restaurantes $restaurantes)
+    {
+        //
+    }
+}
