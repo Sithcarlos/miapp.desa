ecc40d608277dc91515330f30cf2be8f355fe831
Sithcarlos <doncarlos@outlook.com>
2020-08-06 18:26:46 -0500

    correcion, se remueve el uso del modeo en todos los controladores

--- app/Http/Controllers/CategoriasController.php
+++ app/Http/Controllers/CategoriasController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\categorias;
 use Illuminate\Http\Request;
 
 class CategoriasController extends Controller {
--- app/Http/Controllers/PaquetesController.php
+++ app/Http/Controllers/PaquetesController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\paquetes;
 use Illuminate\Http\Request;
 
 class PaquetesController extends Controller {
