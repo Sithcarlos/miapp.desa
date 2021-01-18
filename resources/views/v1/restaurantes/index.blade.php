cac29db1ed7f3e0f55f3dee26eae87b6bb2b81e7
Sithcarlos <doncarlos@outlook.com>
2021-01-18 15:26:44 -0600

    Se agrega boton editar a la pantalla restaurantes listado

--- resources/views/v1/restaurantes/index.blade.php
+++ resources/views/v1/restaurantes/index.blade.php
@@ -9,10 +9,12 @@
             <table class="table table-striped">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
+                        <th scope="col">ID</th>
                         <th scope="col">Nombre</th>
                         <th scope="col">Descripción</th>
                         <th scope="col">Activo</th>
+                        <th scope="col">Imagen</th>
+                        <th scope="col"> Acciones </th>
                     </tr>
                 </thead>
                 <tbody>
@@ -21,11 +23,18 @@
                         <th scope="row"> {{ $rowres->id }} </th>
                         <td> {{ $rowres->nombre }} </td>
                         <td> {{ $rowres->descripcion }} </td>
-                        <td> {{ $rowres->activo }} </td>
+                        <td> {{ ($rowres->activo)? 'SI':'NO' }} </td>
+                        <td> img </td>
+                        <td>
+                            <a href="{{ url('/restaurantes/'.$rowres->id) }}" type="button"
+                                class="btn btn-warning btn-sm">Editar</a>
+                        </td>
                     </tr>
                     @empty
                     <tr>
-                        <th colspan="4"> <p class="text-center">NO HAY RESTAURANTES REGISTRADOS</p> </th>
+                        <th colspan="4">
+                            <p class="text-center">NO HAY RESTAURANTES REGISTRADOS</p>
+                        </th>
 
                     </tr>
 
